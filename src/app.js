e3423050e31a78ec74bc749c5c7bc74b3daae0a5
erwin <mail@antontranelis.de>
2021-10-05 09:32:54 +0200

    map auto height, static ressources

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import {
   TileLayer,
   PlaceMarker
 } from "./utopiaMap.js"
+
+// import * as M from "../node_modules/@materializecss/materialize/dist/js/materialize.min.js"
 // import "thePlugin"
 // import "theOtherPlugin"
 
@@ -11,17 +13,17 @@ const L = window.L
 //const thePlugin = L.thePlugin()
 //const theOtherPlugin = new L.Control.theOtherPlugin(options)
 
-const mymap = new Map('leafletmap').setView([51.505, -0.09], 13);
+const mymap = new Map('leafletmap').setView([51, 9.5], 6);
 //theOtherPlugin.addTo(map)
 new TileLayer('https://tile.osmand.net/hd/{z}/{x}/{y}.png', {
   attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
   maxZoom: 18,
-
 }).addTo(mymap);
 
+populateMap()
 
-
-new PlaceMarker([51.5, -0.09]).addTo(mymap);
-
+function populateMap(){
+  new PlaceMarker([51.5, -0.09]).addTo(mymap);
+}
 
 console.log("app.js working");
--- src/utopiaMap.js
+++ src/utopiaMap.js
@@ -19,4 +19,19 @@ export {
 
 
 
-console.log("utopiaMap.js working");
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  M.FloatingActionButton.init(elems, {hoverEnabled: false});
+  var el = document.querySelectorAll(".dropdown-trigger");
+  M.Dropdown.init(el,{coverTrigger:false, hover: false});
+	setMapHeight();
+});
+
+window.addEventListener('resize', function () {setMapHeight();})
+window.addEventListener('touchmove', function () {setMapHeight();})
+
+function setMapHeight() {
+	var window_height = window.innerHeight;
+	var header_height = document.getElementById("navbar").clientHeight;
+	document.getElementById("leafletmap").style.height=window_height-header_height+"px";
+}
