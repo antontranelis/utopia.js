cf83fb4e751e1c5c2c3123afc44df6a34eabd007
erwin <mail@antontranelis.de>
2021-10-27 13:03:45 +0200

    included gun.js

--- src/Map/map.js
+++ src/Map/map.js
@@ -1,57 +1,14 @@
-import {
-  Control,
-  Map,
-  TileLayer,
-  geoJSON
-} from "../Libraries/myLeaflet.js"
-
-import {
-  places,
-  events,
-  profiles
-} from "../../sampleData/data.js"
-import {
-  PlacePopup,
-  EventPopup,
-  ProfilePopup
-} from "./popups.js"
-import {
-  PlaceMarker,
-  EventMarker,
-  ProfileMarker
-} from "./markers.js"
+import {Control, Map, TileLayer} from "../Libraries/myLeaflet.js"
+import {PlacePopup, EventPopup, ProfilePopup} from "./popups.js"
+import {PlaceMarker, EventMarker, ProfileMarker} from "./markers.js"
 
 
 let UtopiaMap = Map.extend({
-  populateMap: function() {
-    geoJSON(places, {
-      pointToLayer: function(feature, latlng) {
-        return new PlaceMarker(latlng);
-      },
-      onEachFeature: function(feature, layer) {
-        layer.bindPopup(new PlacePopup(feature));
-      }
-    }).addTo(this);
-
-    geoJSON(events, {
-      pointToLayer: function(feature, latlng) {
-        return new EventMarker(latlng);
-      },
-      onEachFeature: function(feature, layer) {
-        layer.bindPopup(new EventPopup(feature));
-      }
-    }).addTo(this);
-
-    geoJSON(profiles, {
-      pointToLayer: function(feature, latlng) {
-        return new ProfileMarker(latlng);
-      },
-      onEachFeature: function(feature, layer) {
-        layer.bindPopup(new ProfilePopup(feature));
-      }
-    }).addTo(this);
-
-
+  addPlace: function(lat, lng, name){
+    new PlaceMarker([lat,lng]).bindPopup(name).addTo(this);
+  },
+  addEvent: function(lat, lng, name){
+    new EventMarker([lat,lng]).bindPopup(name).addTo(this);
   }
 });
 
@@ -64,32 +21,4 @@ UtopiaMap.addInitHook(function() {
 
 export {
   UtopiaMap,
-}
-
-
-
-document.addEventListener('DOMContentLoaded', function() {
-  var elems = document.querySelectorAll('.fixed-action-btn');
-  M.FloatingActionButton.init(elems, {
-    hoverEnabled: false
-  });
-  var el = document.querySelectorAll(".dropdown-trigger");
-  M.Dropdown.init(el, {
-    coverTrigger: false,
-    hover: false
-  });
-  setMapHeight();
-});
-
-window.addEventListener('resize', function() {
-  setMapHeight();
-})
-window.addEventListener('touchmove', function() {
-  setMapHeight();
-})
-
-function setMapHeight() {
-  var window_height = window.innerHeight;
-  var header_height = document.getElementById("navbar").clientHeight;
-  document.getElementById("leafletmap").style.height = window_height - header_height + "px";
 }
--- src/app.js
+++ src/app.js
@@ -1,20 +1,40 @@
-import {
-  UtopiaMap
-} from "./Map/map.js"
-
+import { UtopiaMap } from "./Map/map.js"
+import * as GUN from "../node_modules/gun/gun.js";
 import { config } from "../config/appConfig.js"
-
 // import * as M from "../node_modules/@materializecss/materialize/dist/js/materialize.min.js"
-// import "thePlugin"
-// import "theOtherPlugin"
 
 // eslint-disable-next-line no-unused-vars
 const L = window.L
-//const thePlugin = L.thePlugin()
-//const theOtherPlugin = new L.Control.theOtherPlugin(options)
 
 const map = new UtopiaMap('leafletmap').setView(config.position, config.zoom);
-//theOtherPlugin.addTo(map)
 
+var gun = Gun('http://localhost:8765/gun');
+var places = gun.get('54235555555555555555555555555555555344444');
+var events = gun.get('542355555555555555555554345345555555344444');
 
-map.populateMap();
+places.map().once(item => map.addPlace(item.lat, item.lng, item.text));
+events.map().once(item => map.addEvent(item.lat, item.lng, item.text));
+
+// Event Listeners
+
+document.addEventListener('DOMContentLoaded', (event) => {
+  var elems = document.querySelectorAll('.fixed-action-btn');
+  M.FloatingActionButton.init(elems, {
+    hoverEnabled: false
+  });
+  var el = document.querySelectorAll(".dropdown-trigger");
+  M.Dropdown.init(el, {
+    coverTrigger: false,
+    hover: false
+  });
+  setMapHeight();
+});
+
+window.addEventListener('resize', (event) => setMapHeight())
+window.addEventListener('touchmove', (event) =>  setMapHeight())
+
+function setMapHeight() {
+  var window_height = window.innerHeight;
+  var header_height = document.getElementById("navbar").clientHeight;
+  document.getElementById("leafletmap").style.height = window_height - header_height + "px";
+}
